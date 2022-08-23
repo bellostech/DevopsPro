254d94f05cf2e2f86d307cdc5aeab8a00279ac11
Ubuntu <ubuntu@ip-172-31-16-166.ec2.internal>
2022-08-23 02:40:35 +0000

    updated bash

--- bash.sh
+++ bash.sh
@@ -5,6 +5,7 @@ apt install -y docker.io
 apt install -y default-jre
 apt install -y default-jdk
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 apt update
 apt install -y jenkins
 systemctl start jenkins
