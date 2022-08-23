dbe0f5e9c7966b2e6a86c23c723c4c1a03bb39f8
Ubuntu <ubuntu@ip-172-31-16-166.ec2.internal>
2022-08-23 02:37:28 +0000

    updated bash

--- bash.sh
+++ bash.sh
@@ -4,6 +4,8 @@ apt update
 apt install -y docker.io
 apt install -y default-jre
 apt install -y default-jdk
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+
 apt install -y jenkins
 systemctl start jenkins
 usermod -aG docker jenkins
